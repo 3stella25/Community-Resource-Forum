3e1566a3ee7208f7ce762b226d49638042c5ad2a
Sloan Finger <hello@sloanfinger.com>
2026-02-04 01:26:50 -0500

    formatting fixes

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -3,7 +3,7 @@ import type { profiles } from "~/server/db/schema/tables";
 
 export default function Avatar(profile: (typeof profiles)["$inferSelect"]) {
   return (
-    <Root className="inline-flex size-[1em] items-center justify-center overflow-hidden rounded-full border border-gray-900 bg-gradient-to-br from-sky-400 to-sky-500 align-middle shadow-xs select-none">
+    <Root className="inline-flex size-[1em] items-center justify-center overflow-hidden rounded-full border border-gray-900 bg-linear-to-br from-sky-400 to-sky-500 align-middle shadow-xs select-none">
       <Image
         alt={profile.name ?? "The current signed-in user"}
         className="size-full rounded-[inherit] object-cover"
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -37,7 +37,7 @@ export default async function Navigation() {
     },
   });
 
-  console.log({session});
+  console.log({ session });
 
   const profiles = session
     ? [
